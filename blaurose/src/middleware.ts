6600dba3df07157dac9668ad2ceb9c02841b4e58
ranjero <jjhhssuueeaa@gmail.com>
2024-10-18 23:43:35 +0900

    env, middleware

--- blaurose/src/middleware.ts
+++ blaurose/src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: "/((?!auth|_next/static|_next/images|images|icons|png|favicon.ico|manifest.json).*)",
+  matcher: "/((?!auth|_next/static|_next/images|images|icons|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|favicon.ico|manifest.json).*)",
 };
